fa250689d87b0fb5e1626e4060a36daeb99809bf
waxz <wz67766@163.com>
2017-09-13 16:18:09 +0800

    fix chassis collision

---
+++ update_model.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cp -r prius_description /tmp/workspace/src
+#cp -r prius_msgs /tmp/workspace/src
+# cp -r car_demo /tmp/workspace/src
+#cd /tmp/workspace && source /opt/ros/kinetic/setup.bash&& catkin_make
+
+source /tmp/workspace/devel/setup.bash
+roslaunch car_demo demo.launch
